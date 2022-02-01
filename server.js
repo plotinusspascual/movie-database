2b8ab31ddc3552b6f4b936d8b487a9b9c87f7c53
plotinusspascual <plotinusspascual@gmail.com>
2022-02-01 14:35:30 -0800

    added a post function to add movies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-
+const { response } = require('express');
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
 // Initialize our app variable by setting it to the value of express()
 const app = express();
 // listens to server port or if no server use localhost 3001
@@ -11,53 +11,49 @@ const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "Develop/public")));
 
-// links to db.json to add notes later on
-const notes = require("./Develop/db/db.json");
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'Plotinuss2!',
+    database: 'movie_db'
+  },
+  console.log(`Connected to the movie_db database.`)
+);
 
-// path to api notes and adds to notes
-// res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get("/api/notes", (req, res) => {
-  res.json(notes.slice(1));
-});
-
-// gets back index.html when no query parameters is entered in url
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
-});
-
-// gets back notes.html when query paramter is entered
-app.get("*", (req, res) =>{
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
-});
-
-// create function to get user input in notes and save it in db.json
-function getNotes(body, notesArray) {
-  const newNote = body;
-  if (!Array.isArray(notesArray))
-      notesArray = [];
-  
-  if (notesArray.length === 0)
-      notesArray.push(0);
-
-  body.id = notesArray[0];
-  notesArray[0]++;
-
-  notesArray.push(newNote);
-  fs.writeFileSync(
-      path.join(__dirname, './db/db.json'),
-      JSON.stringify(notesArray, null, 2)
-  );
-  return newNote;
-}
-// make a post function to post saved json
-app.post("/api/notes", (req,res) =>{
-  const newNote = getNotes(req.body, notes);
-  res.json(newNote);
-});
-
-// hosts js in port
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+});
+// SELECT movies.id, title, movie_reviews
+// FROM movies
+// JOIN reviews ON reviews.movie_id = movies.id;
+
+// Query database
+
+
+// Get movie names
+app.get("/api/movies", (req, res) => {
+  db.query('SELECT * FROM movies', 
+    function (err, results) {
+      res.json(results);
+    });
+});
+
+// Add movie
+app.post("/api/add-movies", ({body}, res) => {
+  const sql = "INSERT INTO movies (title) VALUES (?)";
+  const params = [body.title];
+
+  db.query(sql, params, (err, result) => {
+    res.json(result);
+  })
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
 });
